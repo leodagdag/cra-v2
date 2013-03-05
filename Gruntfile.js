6fe339337f8aab97ca263af2263599f19c1f640f
Frederic Patin <f.patin@genesis-groupe.com>
2013-03-05 18:54:20 +0100

    Add Grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: ['<%= pkg.build.path %>', '<%= pkg.dist.path + pkg.dist.file %>'],
+		concat: {
+			options: {
+				// define a string to put between each file in the concatenated output
+				separator: ';'
+			},
+			dist: {
+				// the files to concatenate
+				src: ['<%= pkg.src %>'],
+				// the location of the resulting JS file
+				dest: '<%= pkg.build.path + pkg.build.file %>'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> | <%= pkg.version %> | <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
+			},
+			dist: {
+				files: {
+					'<%= pkg.dist.path + pkg.dist.file %>': ['<%= concat.dist.dest %>']
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', ['clean']);
+	grunt.registerTask('build', ['clean', 'concat']);
+	grunt.registerTask('dist', ['clean', 'concat', 'uglify']);
+
+};
