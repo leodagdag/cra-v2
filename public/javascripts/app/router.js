63be07501e9269201489a91a97c123b94cb076ba
Frederic Patin <f.patin@genesis-groupe.com>
2013-03-11 19:40:05 +0100

    [Global) Add changelog

--- public/javascripts/app/router.js
+++ public/javascripts/app/router.js
@@ -11,62 +11,66 @@ app.config(['$routeProvider',
 	function($routeProvider) {
 
 		$routeProvider
-			.when("/cra/:username/:year/:month", {
-				templateUrl: "public/html/views/cra/cra.html",
+			.when('/cra/:username/:year/:month', {
+				templateUrl: 'public/html/views/cra/cra.html',
 				controller: 'CraCtrl',
 				resolve: {'profile': 'Profile' }
 			})
-			.when("/cra/:username", {
-				templateUrl: "public/html/views/cra/cra.html",
+			.when('/cra/:username', {
+				templateUrl: 'public/html/views/cra/cra.html',
 				controller: 'CraCtrl',
 				resolve: {'profile': 'Profile' }
 			})
-			.when("/cra", {
-				templateUrl: "public/html/views/cra/cra.html",
+			.when('/cra', {
+				templateUrl: 'public/html/views/cra/cra.html',
 				controller: 'CraCtrl',
 				resolve: {'profile': 'Profile' }
 			})
-			.when("/day/:username/:craId/:year/:month/:days", {
-				templateUrl: "public/html/views/cra/day.html",
+			.when('/day/:username/:craId/:year/:month/:days', {
+				templateUrl: 'public/html/views/cra/day.html',
 				controller: 'DayCtrl',
 				resolve: {'profile': 'Profile'}
 			})
-			.when("/absence", {
-				templateUrl: "public/html/views/absence/absence.html",
+			.when('/absence', {
+				templateUrl: 'public/html/views/absence/absence.html',
 				controller: 'AbsenceCtrl',
 				resolve: {'profile': 'Profile' }
 			})
-			.when("/absence/:subSection", {
-				templateUrl: "public/html/views/absence/absence.html",
+			.when('/absence/:subSection', {
+				templateUrl: 'public/html/views/absence/absence.html',
 				controller: 'AbsenceCtrl',
 				resolve: {'profile': 'Profile' }
 			})
-			.when("/claim", {
-				templateUrl: "public/html/views/claim/claim.html",
+			.when('/claim', {
+				templateUrl: 'public/html/views/claim/claim.html',
 				controller: 'ClaimCtrl',
                 resolve: {'profile': 'Profile' }
 			})
-			.when("/remuneration", {
-				templateUrl: "public/html/views/remuneration/remuneration.html",
+			.when('/remuneration', {
+				templateUrl: 'public/html/views/remuneration/remuneration.html',
 				controller: 'RemunerationCtrl'
 			})
-			.when("/parameter", {
-				templateUrl: "public/html/views/parameter/parameter.html",
+			.when('/parameter', {
+				templateUrl: 'public/html/views/parameter/parameter.html',
 				controller: 'ParameterCtrl'
 			})
-			.when("/backoffice", {
-				templateUrl: "public/html/views/back-office/back-office.html",
+			.when('/backoffice', {
+				templateUrl: 'public/html/views/back-office/back-office.html',
 				controller: 'BackOfficeCtrl'
 			})
-			.when("/my-account", {
-				templateUrl: "public/html/views/my-account/my-account.html",
+			.when('/my-account', {
+				templateUrl: 'public/html/views/my-account/my-account.html',
 				controller: 'MyAccountCtrl',
 				resolve: {'profile': 'Profile' }
 			})
-			.when("/my-account/:subSection", {
-				templateUrl: "public/html/views/my-account/my-account.html",
+			.when('/my-account/:subSection', {
+				templateUrl: 'public/html/views/my-account/my-account.html',
 				controller: 'MyAccountCtrl',
 				resolve: {'profile': 'Profile' }
+			})
+			.when('/',{
+				templateUrl:'public/html/views/changeLog.html',
+				controller: 'MainCtrl'
 			});
 
 
