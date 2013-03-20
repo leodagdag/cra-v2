808d7430c59472d98057b7ebee820e643cb2d0f9
Frederic Patin <f.patin@genesis-groupe.com>
2013-03-20 19:29:46 +0100

    Save

--- public/javascripts/app/controllers/account/PartTimeCtrl.js
+++ public/javascripts/app/controllers/account/PartTimeCtrl.js
@@ -2,15 +2,15 @@ app.controller('PartTimeNewCtrl', ['$rootScope', '$scope', '$http', '$log', '$lo
 	function PartTimeNewCtrl($rootScope, $scope, $http, $log, $location) {
 		$scope._ = _;
 		/* Form */
-		var PartTime = function (form) {
+		var PartTime = function(form) {
 			this.userId = $scope.profile.id;
-			this.startDate = moment(form.startDate, 'DD/MM/YYYY').valueOf();
+			this.startDate = (form.startDate) ? moment(form.startDate, 'DD/MM/YYYY').valueOf() : null;
 			this.endDate = (form.endDate) ? moment(form.endDate, 'DD/MM/YYYY').valueOf() : null;
 			this.daysOfWeek = form.daysOfWeek;
 			this.frequency = form.frequency;
 		};
 
-		var Form = function () {
+		var Form = function() {
 			this.startDate = null;
 			this.endDate = null;
 			this.daysOfWeek = [];
@@ -33,20 +33,20 @@ app.controller('PartTimeNewCtrl', ['$rootScope', '$scope', '$http', '$log', '$lo
 
 		$scope.form = new Form();
 
-		$scope.toggleDay = function (dayId, dayMoment) {
-			if (dayMoment) {
+		$scope.toggleDay = function(dayId, dayMoment) {
+			if(dayMoment) {
 				var weekDay = {"dayOfWeek": dayId, "momentOfDay": dayMoment.moment};
 				$scope.form.daysOfWeek.push(weekDay);
 			} else {
 				$scope.form.daysOfWeek = _($scope.form.daysOfWeek)
-					.reject(function (item) {
+					.reject(function(item) {
 						return item.dayOfWeek === this.valueOf();
 					}, dayId)
 					.valueOf();
 			}
 		};
 
-		$scope.save = function () {
+		$scope.save = function() {
 			var partTime = new PartTime($scope.form);
 			var route = jsRoutes.controllers.JPartTimes.addPartTimes();
 			$http({
@@ -54,12 +54,12 @@ app.controller('PartTimeNewCtrl', ['$rootScope', '$scope', '$http', '$log', '$lo
 				url: route.url,
 				data: partTime
 			})
-				.success(function (data, status, headers, config) {
+				.success(function(data, status, headers, config) {
 					$rootScope.onSuccess("Votre temps partiel a été sauvegardé.");
 					$scope.loadActive();
 				})
-				.error(function (errors, status, headers, config) {
-					_(errors).forEach(function (err, key) {
+				.error(function(errors, status, headers, config) {
+					_(errors).forEach(function(err, key) {
 						$log.log('err', err);
 						$scope.errors[key] = err.join('<br>');
 					});
@@ -69,26 +69,26 @@ app.controller('PartTimeNewCtrl', ['$rootScope', '$scope', '$http', '$log', '$lo
 		/* Active PartTime */
 		$scope.activePartTime = {};
 
-		$scope.loadActive = function () {
+		$scope.loadActive = function() {
 			var route = jsRoutes.controllers.JPartTimes.active($scope.profile.id);
 			$http({
 				method: route.method,
 				url: route.url
 			})
-				.success(function (activePartTime, status, headers, config) {
+				.success(function(activePartTime, status, headers, config) {
 					$scope.activePartTime = activePartTime;
 				});
 		};
 
-		$scope.deactivate = function (id) {
-			if (confirm('Êtes vous sur de vouloir désactivé votre temps partiel ?')) {
+		$scope.deactivate = function(id) {
+			if(confirm('Êtes vous sur de vouloir désactivé votre temps partiel ?')) {
 				var route = jsRoutes.controllers.JPartTimes.deactivate();
 				$http({
 					method: route.method,
 					url: route.url,
 					data: {'id': id}
 				})
-					.success(function (result, status, headers, config) {
+					.success(function(result, status, headers, config) {
 						$scope.activePartTime = {};
 						$rootScope.onSuccess("Le temps partiel a été désactivé.");
 					});
