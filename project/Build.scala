951e9574da0580a171cf970047068ffc4452f6b7
Frederic Patin <f.patin@genesis-groupe.com>
2013-04-01 21:35:57 +0200

    Prepare for next release[2.0.18-SNAPSHOT]

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
 	val appName = "cra-v2"
-	val appVersion = "2.0.17"
+	val appVersion = "2.0.18-SNAPSHOT"
 
 	val appDependencies = Seq(
 		// Add your project dependencies here,
